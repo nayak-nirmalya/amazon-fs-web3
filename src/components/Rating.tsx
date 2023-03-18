7c74f268da99c7c1db2385439a63faffc48e10fa
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-18 22:41:03 +0530

    added custom svgs and showing rating

---
+++ src/components/Rating.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import star_regular from "../assets/star-regular.svg";
+import star_solid from "../assets/star-solid.svg";
+
+interface RatingProps {
+  value: number;
+}
+
+const Rating: React.FC<RatingProps> = ({ value }) => {
+  return (
+    <div className="rating">
+      <img
+        src={value >= 1 ? star_solid : star_regular}
+        width="20px"
+        height="20px"
+        alt="Star"
+      />
+      <img
+        src={value >= 2 ? star_solid : star_regular}
+        width="20px"
+        height="20px"
+        alt="Star"
+      />
+      <img
+        src={value >= 3 ? star_solid : star_regular}
+        width="20px"
+        height="20px"
+        alt="Star"
+      />
+      <img
+        src={value >= 4 ? star_solid : star_regular}
+        width="20px"
+        height="20px"
+        alt="Star"
+      />
+      <img
+        src={value >= 5 ? star_solid : star_regular}
+        width="20px"
+        height="20px"
+        alt="Star"
+      />
+    </div>
+  );
+};
+
+export default Rating;
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { ethers } from "ethers";
 import { Item } from "../types";
+import Rating from "./Rating";
 
 interface SectionProps {
   title: string;
@@ -19,6 +21,11 @@ const Section: React.FC<SectionProps> = ({ title, items, togglePop }) => {
           <div className="card" key={index} onClick={() => togglePop()}>
             <div className="card__image">
               <img src={item.image} alt={item.name} />
+            </div>
+            <div className="card__info">
+              <h4>{item.name}</h4>
+              <Rating value={item.rating as unknown as number} />
+              <p>{ethers.formatUnits(item.cost.toString(), "ether")} ETH</p>
             </div>
           </div>
         ))}
