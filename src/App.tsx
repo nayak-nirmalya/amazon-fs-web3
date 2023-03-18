dd7bdf1dd0b01ffa1058113a91663157613a8fec
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-18 19:15:50 +0530

    Contract Deployed Network Address with config.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+// Contract Deployed Network and Address
+import config from "./config.json";
+
+// Import Smart Contract ABI from Backend (HardHat) Folder.
+import { abi } from "../../amazon-hardhat-backend/artifacts/contracts/AmazonDApp.sol/AmazonDApp.json";
+
 function App() {
+  const loadBlockchainData = async () => {
+    console.log("Loading...");
+  };
   return (
     <div>
       <h2>Hello World!</h2>
